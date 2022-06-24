78c46f9ad87041b07892748c0d47c401c8b8722f
David <davidolanrewaju538@gmail.com>
2022-06-24 23:15:50 +0100

    Migrated

--- I4G004596FNA/blog/views.py
+++ I4G004596FNA/blog/views.py
@@ -1,3 +1,4 @@
+from django.forms import SlugField
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -10,18 +11,19 @@ class PostListView(generic.ListView):
 
 class PostCreateView(generic.CreateView):
     model = Post
-    fields = ["__all__"]
+    fields = "__all__"
     success_url = reverse_lazy("blog:all")
 
 class PostDetailView(generic.DetailView):
     model = Post
 
 class PostUpdateView(generic.UpdateView):
-    model: Post
-    fields = ["__all__"]
+    model = Post
+    fields = "__all__"
     success_url: reverse_lazy("blog:all")
 
 class PostDeleteView(generic.DeleteView):
     model: Post
-    fields = ["__all__"]
+    fields = "__all__"
     success_url: reverse_lazy("blog:all")
+     
